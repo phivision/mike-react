a02debd0381c0ee359e27cf9aef1a2e2506bf4f8
Fanghao Yang <yangfanghao@gmail.com>
2021-02-25 16:31:56 -0500

    amplify integration

---
+++ src/graphql/mutations.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createUserProfile = /* GraphQL */ `
+  mutation CreateUserProfile($input: CreateUserProfileInput!) {
+    createUserProfile(input: $input) {
+      UserID
+      UserRole
+      UserName
+      RegDate
+      UserImage
+      Gender
+      Birthday
+      Height
+      Weight
+      Price
+      Email
+      StripeID
+    }
+  }
+`;
+export const updateUserProfile = /* GraphQL */ `
+  mutation UpdateUserProfile($input: UpdateUserProfileInput!) {
+    updateUserProfile(input: $input) {
+      UserID
+      UserRole
+      UserName
+      RegDate
+      UserImage
+      Gender
+      Birthday
+      Height
+      Weight
+      Price
+      Email
+      StripeID
+    }
+  }
+`;
+export const deleteUserProfile = /* GraphQL */ `
+  mutation DeleteUserProfile($input: DeleteUserProfileInput!) {
+    deleteUserProfile(input: $input) {
+      UserID
+      UserRole
+      UserName
+      RegDate
+      UserImage
+      Gender
+      Birthday
+      Height
+      Weight
+      Price
+      Email
+      StripeID
+    }
+  }
+`;
