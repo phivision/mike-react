c8abce0239c0f9edaa8681c624f9d7856779610c
Fanghao Yang <yangfanghao@gmail.com>
2021-03-04 14:00:25 -0500

    get user profile and update on frontend table

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, {Auth, API, graphqlOperation} from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsconfig from './aws-exports';
+import { getUserProfile } from './graphql/queries';
 
 Amplify.configure(awsconfig);
+// Use ID token instead of access token in API calls
+Amplify.configure({
+    API: {
+        graphql_headers: async () => {
+            const session = await Auth.currentSession();
+            return {
+                Authorization: session.getIdToken().getJwtToken(),
+            };
+        },
+    },
+});
+const initialProfileState = {
+    id: '',
+    LastName: null,
+    FirstName: null,
+    UserImage: null,
+    RegDate: '',
+    UserRole: '',
+    Birthday: null,
+    Email: null,
+    Gender: null,
+    Height: null,
+    Weight: null,
+    Price: null,
+    StripID: null
+}
 
 const App = () => {
     const [authState, setAuthState] = React.useState();
     const [user, setUser] = React.useState();
+    const [profile, setProfile] = React.useState(initialProfileState);
 
     React.useEffect(() => {
         return onAuthUIStateChange((nextAuthState, authData) => {
@@ -18,9 +46,26 @@ const App = () => {
         });
     }, []);
 
+    // check if user exist in DynamoDB, if not, create with cognito key
+    async function userQuery(){
+        const userProfile = await API.graphql(graphqlOperation(getUserProfile, {id: user.username}));
+        if(userProfile.data.getUserProfile != null) {
+            setProfile(userProfile.data.getUserProfile)
+        }else{
+            alert('cannot find user profile!')
+        }
+    }
+
     return authState === AuthState.SignedIn && user ? (
         <div className="App">
             <div>Hello, {user.username}</div>
+            <button onClick={userQuery}>Get Profile</button>
+            <div style={{marginBottom: 30}}>
+                <div key={profile.Email}>
+                    <p>{profile.RegDate}</p>
+                    <p>{profile.UserRole}</p>
+                </div>
+            </div>
             <AmplifySignOut />
         </div>
     ) : (
@@ -45,6 +90,13 @@ const App = () => {
                         type: "phone_number",
                         label: "Phone number",
                         placeholder: "your phone number",
+                        required: true,
+                    },
+                    // TODO: change the fill-in slot to drop down menu so that the user could only select from options
+                    {
+                        type: "custom:role",
+                        label: "Role",
+                        placeholder: "your platform role",
                         required: true,
                     },
                 ]}
