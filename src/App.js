e05c86bcb8714d70796b4b1a9c267e997aa72f83
Fanghao Yang <yangfanghao@gmail.com>
2021-03-05 16:02:50 -0500

    1. add Material UI frontend 2. integrate with Amplify UI

--- src/App.js
+++ src/App.js
@@ -1,109 +1,76 @@
-import React from 'react';
-import './App.css';
-import Amplify, {Auth, API, graphqlOperation} from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignUp, AmplifySignIn, AmplifySignOut } from '@aws-amplify/ui-react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import awsconfig from './aws-exports';
-import { getUserProfile } from './graphql/queries';
+import React from "react";
+import "./App.css";
+import Amplify from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifySignUp,
+  AmplifySignIn,
+} from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
+import { createBrowserHistory } from "history";
+import { BrowserRouter as Router, Redirect } from "react-router-dom";
+// core components
+import Admin from "layouts/Admin.js";
 
+import "assets/css/material-dashboard-react.css?v=1.9.0";
+
+const hist = createBrowserHistory();
+// amplify config
 Amplify.configure(awsconfig);
-// Use ID token instead of access token in API calls
-Amplify.configure({
-    API: {
-        graphql_headers: async () => {
-            const session = await Auth.currentSession();
-            return {
-                Authorization: session.getIdToken().getJwtToken(),
-            };
-        },
-    },
-});
-const initialProfileState = {
-    id: '',
-    LastName: null,
-    FirstName: null,
-    UserImage: null,
-    RegDate: '',
-    UserRole: '',
-    Birthday: null,
-    Email: null,
-    Gender: null,
-    Height: null,
-    Weight: null,
-    Price: null,
-    StripID: null
-}
 
 const App = () => {
-    const [authState, setAuthState] = React.useState();
-    const [user, setUser] = React.useState();
-    const [profile, setProfile] = React.useState(initialProfileState);
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
 
-    React.useEffect(() => {
-        return onAuthUIStateChange((nextAuthState, authData) => {
-            setAuthState(nextAuthState);
-            setUser(authData)
-        });
-    }, []);
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
 
-    // check if user exist in DynamoDB, if not, create with cognito key
-    async function userQuery(){
-        const userProfile = await API.graphql(graphqlOperation(getUserProfile, {id: user.username}));
-        if(userProfile.data.getUserProfile != null) {
-            setProfile(userProfile.data.getUserProfile)
-        }else{
-            alert('cannot find user profile!')
-        }
-    }
-
-    return authState === AuthState.SignedIn && user ? (
-        <div className="App">
-            <div>Hello, {user.username}</div>
-            <button onClick={userQuery}>Get Profile</button>
-            <div style={{marginBottom: 30}}>
-                <div key={profile.Email}>
-                    <p>{profile.RegDate}</p>
-                    <p>{profile.UserRole}</p>
-                </div>
-            </div>
-            <AmplifySignOut />
-        </div>
-    ) : (
-        <AmplifyAuthenticator usernameAlias="email">
-            <AmplifySignUp
-                slot="sign-up"
-                usernameAlias="email"
-                formFields={[
-                    {
-                        type: "email",
-                        label: "Email",
-                        placeholder: "your email address",
-                        required: true,
-                    },
-                    {
-                        type: "password",
-                        label: "Password",
-                        placeholder: "your password",
-                        required: true,
-                    },
-                    {
-                        type: "phone_number",
-                        label: "Phone number",
-                        placeholder: "your phone number",
-                        required: true,
-                    },
-                    // TODO: change the fill-in slot to drop down menu so that the user could only select from options
-                    {
-                        type: "custom:role",
-                        label: "Role",
-                        placeholder: "your platform role",
-                        required: true,
-                    },
-                ]}
-            />
-            <AmplifySignIn slot="sign-in" usernameAlias="email" />
-        </AmplifyAuthenticator>
-    );
-}
+  return authState === AuthState.SignedIn && user ? (
+    <Router history={hist}>
+      <Admin user={user} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Router>
+  ) : (
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignUp
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[
+          {
+            type: "email",
+            label: "Email",
+            placeholder: "your email address",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Password",
+            placeholder: "your password",
+            required: true,
+          },
+          {
+            type: "phone_number",
+            label: "Phone number",
+            placeholder: "your phone number",
+            required: true,
+          },
+          // TODO: change the fill-in slot to drop down menu so that the user could only select from options
+          {
+            type: "custom:role",
+            label: "Role",
+            placeholder: "your platform role",
+            required: true,
+          },
+        ]}
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
+    </AmplifyAuthenticator>
+  );
+};
 
 export default App;
