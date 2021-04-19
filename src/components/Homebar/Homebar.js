b0c284d6b6f58636ae20d494ef4c1a010541b89f
Xin Wu <xinwuwh@gmail.com>
2021-04-19 18:42:47 +0800

    remove comments and add style files

--- src/components/Homebar/Homebar.js
+++ src/components/Homebar/Homebar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
-// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
 import styles from "assets/jss/material-dashboard-react/components/headerStyle.js";
@@ -26,7 +25,6 @@ const SearchButton = styled(SearchBar)`
 export default function Header() {
   const classes = useStyles();
   let history = useHistory();
-
   const [query, setQuery] = React.useState("");
   return (
     <AppBar className={classes.appBar}>
