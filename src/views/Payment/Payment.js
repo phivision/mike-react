b9b60cd18ea78d801a11bc1425afaae0e83b2244
Thomas Wang <wang.thomasdavid@gmail.com>
2021-04-20 12:46:32 -0400

    Working onboarding Process

--- src/views/Payment/Payment.js
+++ src/views/Payment/Payment.js
@@ -1,31 +1,55 @@
+import React from "react";
 import { API, Auth } from "aws-amplify";
-// import { useHistory } from "react-router-dom";
+import { getUserProfile } from "../../graphql/queries";
+import { Button } from "@material-ui/core";
 
 export default function Payment() {
-  // const history = useHistory();
+  async function userQuery(cognitoID) {
+    const userProfile = await API.graphql({
+      query: getUserProfile,
+      variables: { id: cognitoID },
+    });
+    if (userProfile.data.getUserProfile != null) {
+      return userProfile.data.getUserProfile;
+    }
+  }
 
+  //TODO: Need to redo refresh + return URLs later, when actually deployed to domain to use window.location.href + logic
   const getLink = async () => {
-    await Auth.currentAuthenticatedUser().then((attributes) => {
-      const myInit = {
-        headers: {}, // AWS-IAM authorization if using empty headers
-        body: {
-          connectedID: attributes.username,
-          refreshUrl: "localhost:3000/admin/dashboard",
-          returnUrl: "localhost:3000/admin/dashboard",
-          // refreshUrl: window.location.href,
-          // returnUrl: window.location.href,
-        },
-        response: true,
-      };
+    await Auth.currentAuthenticatedUser().then(async (attributes) => {
+      await userQuery(attributes.username).then((userProfile) => {
+        const myInit = {
+          headers: {}, // AWS-IAM authorization if using empty headers
+          body: {
+            connectedID: userProfile.StripeID,
+            refreshUrl: "https://www.google.com",
+            returnUrl: "https://www.google.com",
+            // refreshUrl: window.location.href,
+            // returnUrl: window.location.href,
+          },
+          response: true,
+        };
 
-      API.post("stripeAPI", "/stripe/api/onboarding", myInit)
-        .then((res) => console.log(res))
-        .catch((err) => {
-          console.log(err);
-        });
+        API.post("stripeAPI", "/stripe/api/onboarding", myInit)
+          .then((res) => {
+            window.location.href = res.data.AccountLink;
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     });
   };
 
-  getLink();
-  return null;
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          getLink();
+        }}
+      >
+        Stripe Dashboard
+      </Button>
+    </div>
+  );
 }
