d29dbe6925d61727b5aa42e8846ed2511d565a99
Fanghao Yang <yangfanghao@gmail.com>
2021-04-27 16:05:20 -0400

    combine auth view styles and remove redundant code

--- src/views/Auth/SignUp.js
+++ src/views/Auth/SignUp.js
@@ -4,30 +4,15 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { Auth } from "aws-amplify";
 import { Link, useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+// import auth styles
+import authStyles from "../../assets/jss/material-dashboard-react/views/authStyle";
 
 export default function SignUp({ ...props }) {
-  const classes = useStyles();
+  const classes = authStyles();
   const history = useHistory();
 
   const [state, setState] = React.useState({
