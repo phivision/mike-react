351216fb7f618d27854ec28f5a6bd1c27bdad3b6
Thomas Wang <wang.thomasdavid@gmail.com>
2021-04-27 13:42:54 -0400

    Created card form using Stripe Elements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { Hub } from "aws-amplify";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 import { BrowserRouter, Switch, Redirect } from "react-router-dom";
 // core components
@@ -34,19 +36,25 @@ const App = () => {
     });
   }, []);
 
+  const stripePromise = loadStripe(
+    "pk_test_51IWoNlAXegvVyt5sEGxoPrV9MfyryI7OR5vKuY4bLXUgqWIE2Dv0TmtY5R9BVHpjhg3qssoAF3z5GhtkgHrc8Mc400VDRuU2yX"
+  );
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <PublicRoute path="/home" component={Home} />
-        <PrivateRoute
-          path="/admin"
-          user={user}
-          auth={authState}
-          component={Admin}
-        />
-        <Redirect to="/home" />
-      </Switch>
-    </BrowserRouter>
+    <Elements stripe={stripePromise}>
+      <BrowserRouter>
+        <Switch>
+          <PublicRoute path="/home" component={Home} />
+          <PrivateRoute
+            path="/admin"
+            user={user}
+            auth={authState}
+            component={Admin}
+          />
+          <Redirect to="/home" />
+        </Switch>
+      </BrowserRouter>
+    </Elements>
   );
 };
 
--- src/views/Checkout/Checkout.js
+++ src/views/Checkout/Checkout.js
@@ -1,50 +1,37 @@
 import React from "react";
-import { API, Auth } from "aws-amplify";
-import { Button } from "@material-ui/core";
-import PropTypes from "prop-types";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
-export default function Checkout({ ...props }) {
-  //TODO: Need to redo refresh + return URLs later, when actually deployed to domain to use window.location.href + logic
-  const getLink = async () => {
-    await Auth.currentAuthenticatedUser().then((attributes) => {
-      const myInit = {
-        headers: {}, // AWS-IAM authorization if using empty headers
-        body: {
-          customerID: attributes.username,
-          trainerID: props.user,
-          success_url: "https://www.google.com",
-          cancel_url: "https://www.google.com",
-          // success_url: window.location.href,
-          // cancel_url: window.location.href,
-        },
-        response: true,
-      };
+export default function Checkout() {
+  const stripe = useStripe();
+  const elements = useElements();
 
-      API.post("stripeAPI", "/stripe/api/checkout", myInit)
-        .then((res) => {
-          console.log(res);
-          // window.location.href = res.data.AccountLink;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
     });
+    console.log(error);
+    console.log(paymentMethod);
   };
 
   return (
     <div>
-      Hello
-      <Button
-        onClick={() => {
-          getLink();
-        }}
-      >
-        Stripe Checkout
-      </Button>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <CardElement />
+        <button type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
     </div>
   );
 }
-
-Checkout.propTypes = {
-  user: PropTypes.string.isRequired,
-};
