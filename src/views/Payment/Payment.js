b648a20c012174aa3347ef284936674cecdadeca
Thomas Wang <wang.thomasdavid@gmail.com>
2021-04-28 13:58:14 -0400

    Finished redoing link routes

--- src/views/Payment/Payment.js
+++ src/views/Payment/Payment.js
@@ -1,55 +1,77 @@
 import React from "react";
-import { API, Auth } from "aws-amplify";
-import { getUserProfile } from "../../graphql/queries";
+import { API } from "aws-amplify";
 import { Button } from "@material-ui/core";
+import PropTypes from "prop-types";
 
-export default function Payment() {
-  async function userQuery(cognitoID) {
-    const userProfile = await API.graphql({
-      query: getUserProfile,
-      variables: { id: cognitoID },
-    });
-    if (userProfile.data.getUserProfile != null) {
-      return userProfile.data.getUserProfile;
-    }
-  }
+//TODO: replace with Live URL
+export default function Payment({ ...props }) {
+  const onboard = async () => {
+    const myInit = {
+      headers: {}, // AWS-IAM authorization if using empty headers
+      body: {
+        id: props.user,
+        refreshUrl: "http://localhost:3000/admin/dashboard",
+        returnUrl: "http://localhost:3000/admin/dashboard",
+      },
+      response: true,
+    };
 
-  //TODO: Need to redo refresh + return URLs later, when actually deployed to domain to use window.location.href + logic
-  const getLink = async () => {
-    await Auth.currentAuthenticatedUser().then(async (attributes) => {
-      await userQuery(attributes.username).then((userProfile) => {
-        const myInit = {
-          headers: {}, // AWS-IAM authorization if using empty headers
-          body: {
-            connectedID: userProfile.StripeID,
-            refreshUrl: "https://www.google.com",
-            returnUrl: "https://www.google.com",
-            // refreshUrl: window.location.href,
-            // returnUrl: window.location.href,
-          },
-          response: true,
-        };
-
-        API.post("stripeAPI", "/stripe/api/trainer/link/onboarding", myInit)
-          .then((res) => {
-            window.location.href = res.data.AccountLink;
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+    API.post("stripeAPI", "/stripe/api/trainer/link/onboarding", myInit)
+      .then((res) => {
+        window.location.href = res.data.AccountLink;
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    });
+  };
+
+  const login = async () => {
+    const myInit = {
+      headers: {}, // AWS-IAM authorization if using empty headers
+      body: {
+        id: props.user,
+      },
+      response: true,
+    };
+
+    API.post("stripeAPI", "/stripe/api/trainer/link/login", myInit)
+      .then((res) => {
+        window.location.href = res.data.AccountLink;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
       <Button
         onClick={() => {
-          getLink();
+          onboard();
         }}
       >
-        Stripe Dashboard
+        Stripe Onboarding
+      </Button>
+      <Button
+        onClick={() => {
+          login();
+        }}
+      >
+        Stripe Login
       </Button>
     </div>
   );
 }
+
+Payment.propTypes = {
+  user: PropTypes.string,
+};
