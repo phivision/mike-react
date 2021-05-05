9b9e930a69b1a6b01d590467cd3b499ec35585fb
Thomas Wang <wang.thomasdavid@gmail.com>
2021-05-05 11:05:49 -0400

    MK-26 #comment footer grid done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,50 +2,36 @@
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import { ListItem, List, Typography, Link, Container } from "@material-ui/core";
+import { Grid, Link, Typography } from "@material-ui/core";
 // core components
 import styles from "assets/jss/material-dashboard-react/components/footerStyle.js";
 
 const useStyles = makeStyles(styles);
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="http://phivision.us/">
-        PhiVision Inc.
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 export default function Footer() {
   const classes = useStyles();
   // TODO: adding & fixing links to company blog and contact us
   return (
-    <footer className={classes.footer}>
-      <Container className={classes.container}>
-        <List className={classes.list}>
-          <ListItem className={classes.inlineBlock}>
-            <a href="../../home" className={classes.block}>
-              Home
-            </a>
-          </ListItem>
-          <ListItem className={classes.inlineBlock}>
-            <a href="https://wordpress.com/" className={classes.block}>
-              Blog
-            </a>
-          </ListItem>
-          <ListItem className={classes.inlineBlock}>
-            <a href="https://wordpress.com/" className={classes.block}>
-              Contact Us
-            </a>
-          </ListItem>
-        </List>
-        <Copyright />
-      </Container>
+    <footer>
+      <Grid container>
+        <Grid item xs>
+          <Typography variant="body2" color="textSecondary" align="left">
+            {"Copyright © " +
+              new Date().getFullYear() +
+              " PhiVision Inc. All rights reserved."}
+          </Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Link href="https://www.iubenda.com/privacy-policy/84247709">
+            Privacy Policy
+          </Link>
+        </Grid>
+        <Grid item xs={1}>
+          <Link href="https://www.iubenda.com/terms-and-conditions/84247709">
+            Terms of Use
+          </Link>
+        </Grid>
+      </Grid>
     </footer>
   );
 }
