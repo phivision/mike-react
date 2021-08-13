2d991616a534ebf1f9205cf48dc73603467342cd
Thomas Wang <wang.thomasdavid@gmail.com>
2021-08-13 17:09:16 -0400

    Backend Updates

--- amplify/backend/function/stripeAPI/src/prices.js
+++ amplify/backend/function/stripeAPI/src/prices.js
@@ -1,10 +1,12 @@
 //REMEMBER TO UPDATE THE CORRESPONDING PRICES FOR STRIPE WEBHOOK!!
 
 const prices = {
-  "10": 140,
-  "50": 700,
-  "150": 2000,
-  "500": 6500,
+  "10": 199,
+  "50": 999,
+  "100": 1799,
+  "150": 2499,
+  "500": 7499,
+  "1000": 15000,
 };
 
 const conversionRate = 10;
